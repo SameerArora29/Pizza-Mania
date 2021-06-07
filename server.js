381f7766fc047d5d9e5c2fd4699f472893702539
Sameer Arora <sameer.a.arora0@gmail.com>
2021-06-07 23:00:42 +0530

    add cart page,auth

--- server.js
+++ server.js
@@ -6,15 +6,30 @@ const path = require('path');
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
-app.get('/' , function(req,res){
-    res.render('home')
-})
+app.use(express.static('public'))
+
 
 
 // set template engine
 app.use(expressLayout)
 app.set('views' , path.join(__dirname,'/resources/views'))
 app.set('view engine' , 'ejs')
+
+app.get('/' , function(req,res){
+    res.render('home')
+})
+
+app.get('/cart' , function(req,res){
+    res.render('customers/cart')
+})
+
+app.get('/login' , function(req,res){
+    res.render('auth/login')
+})
+app.get('/register' , function(req,res){
+    res.render('auth/register')
+})
+
 app.listen(PORT , function(){
 
 console.log(`listening on port ${PORT}`)
